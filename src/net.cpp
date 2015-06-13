b9d7944c71c573ee6c1273447e26130fd6897450
drwatson84 <drwatson84@users.noreply.github.com>
2015-06-13 05:53:23 +0300

    updated seed

--- src/net.cpp
+++ src/net.cpp
@@ -1173,7 +1173,7 @@ void MapPort(bool)
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strMainNetDNSSeed[][2] = {
-        {"seed.pakcoin.org", "96.126.113.6"}
+        {"seed.pakcoin.org", "seed.pakcoin.org"}
 };
 
 static const char *strTestNetDNSSeed[][2] = {
